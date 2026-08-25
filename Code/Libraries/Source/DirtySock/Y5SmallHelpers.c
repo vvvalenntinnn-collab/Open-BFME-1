31cf26c619a0eea7ceff4f378cee2c59cf8bdcae
vvvalenntinnn-collab <320846852+vvvalenntinnn-collab@users.noreply.github.com>
2026-08-25 06:02:08 -0700

    Reconstruct DirtySock guarded RC4 stream helper

--- Code/Libraries/Source/DirtySock/Y5SmallHelpers.c
+++ Code/Libraries/Source/DirtySock/Y5SmallHelpers.c
@@ -23,3 +23,11 @@ int Rva0080E300(const int *crypto, int length)
 		length -= 8;
 	return length;
 }
+
+void Rva0080F300(void *state, unsigned char *data, int length);
+
+void Rva0080E410(void *crypto, unsigned char *data, int length)
+{
+	if (*(int *)crypto != 0)
+		Rva0080F300((unsigned char *)crypto + 0x10A, data, length);
+}
